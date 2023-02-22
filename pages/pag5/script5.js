57b7e2c101f425e87d8a537620044a7e34650cee
Diogo Otoni <diogootoni@hotmail.com>
2023-02-22 10:50:25 -0300

    Finalizado - passível de alteração

--- pages/pag5/script5.js
+++ pages/pag5/script5.js
@@ -1,55 +1,59 @@
-const paulo = {
-    nome: 'Paulo',
-    vitoria: 0,
-    empate: 0,
-    derrota: 0,
-    pontos: 0  
-}
-  
-const rafa = {
-    nome: 'Rafa',
-    vitoria: 0,
-    empate: 0,
-    derrota: 0,
-    pontos: 0  
-}
-  
 const elementoTabela = document.getElementById('tabelaJogadores');
-  exibirNaTela();
-  
+const elInputJogador = document.getElementById('inputJogador');
+const elPlayGame = document.getElementById('playGame');
+
+const vetorJogadores = [];
+
+function inserirJogador(){
+    insereJogadorNoVetor(elInputJogador.value);
+}
+
+function insereJogadorNoVetor (nome){
+    let jogador = {
+        nome: nome,
+        vitoria: 0,
+        empate: 0,
+        derrota: 0,
+        pontos: 0
+    };
+    vetorJogadores.push(jogador);
+    elInputJogador.value = '';
+}
+
 function exibirNaTela(){
-    elementoTabela.innerHTML = `
+    elementoTabela.innerHTML = '';
+    iniciarJogo();
+}
+
+function iniciarJogo(){
+    elementoTabela.innerHTML = '';
+    for(let i = 0; i < vetorJogadores.length; i++){
+        elementoTabela.innerHTML += `
         <tr>
-          <td>${paulo.nome}</td>
-          <td>${paulo.vitoria}</td>
-          <td>${paulo.empate}</td>
-          <td>${paulo.derrota}</td>
-          <td>${paulo.pontos}</td>
-          <td><button onClick="adicionarVitoria(paulo)">Vitória</button></td>
-          <td><button onClick="adicionarEmpate(paulo)">Empate</button></td>
-          <td><button onClick="adicionarDerrota(paulo)">Derrota</button></td>
+          <td>${vetorJogadores[i].nome}</td>
+          <td>${vetorJogadores[i].vitoria}</td>
+          <td>${vetorJogadores[i].empate}</td>
+          <td>${vetorJogadores[i].derrota}</td>
+          <td>${vetorJogadores[i].pontos}</td>
+          <td><button onClick="adicionarVitoria(vetorJogadores, ${i})">Vitória</button></td>
+          <td><button onClick="adicionarEmpate(vetorJogadores, ${i})">Empate</button></td>
+          <td><button onClick="adicionarDerrota(vetorJogadores, ${i})">Derrota</button></td>
         </tr>`
+    }
 }
   
-function adicionarVitoria(jogador){  
-    jogador.vitoria++;
-    jogador.pontos += 3;
+function adicionarVitoria(vetorJogadores, posicao){
+    vetorJogadores[posicao].vitoria++;
+    vetorJogadores[posicao].pontos += 3;
     exibirNaTela()
 }
-function adicionarEmpate(jogador){
-    jogador.empate++;
-    jogador.pontos += 1;
-    exibirNaTela()
-}
-  
-function adicionarDerrota(jogador){
-    jogador.derrota++;
+function adicionarEmpate(vetorJogadores, posicao){
+    vetorJogadores[posicao].empate++;
+    vetorJogadores[posicao].pontos += 1;
     exibirNaTela()
 }
   
-  
-  //FAZER LISTA COM JOGADORES E FOR PARA EXIBIR TELA, TALVEZ ATÉ CADASTRAR JOGADORES
-  
-  //PERSONALIZAR TABELA: PARA O JOGO QUE GOSTA - KILL, DEATH, KD, VIT, DER, ASSIST, RANK
-  
-  //COMPARTILHAR NAS REDES SOCIAIS
+function adicionarDerrota(vetorJogadores, posicao){
+    vetorJogadores[posicao].derrota++;
+    exibirNaTela()
+}
